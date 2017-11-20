8a614991a541f48db9262954befe7abcef034b18
Ramon de Vaan <ramon.de.vaan@capgemini.com>
2017-11-20 10:44:20 +0100

    Added components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { EmployeeIndexComponent } from './employee-index/employee-index.component';
+import { EmployeeAddComponent } from './employee-add/employee-add.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { EmployeeDeleteComponent } from './employee-delete/employee-delete.component';
+import { EmployeeService } from './employee.service';
+import { ErrorService } from './error.service';
+import { LoggingService } from './logging.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeIndexComponent,
+    EmployeeAddComponent,
+    EmployeeEditComponent,
+    EmployeeDetailComponent,
+    EmployeeDeleteComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [EmployeeService, ErrorService, LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/employee-index/employee-index.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmployeeIndexComponent } from './employee-index.component';
+
+describe('EmployeeIndexComponent', () => {
+  let component: EmployeeIndexComponent;
+  let fixture: ComponentFixture<EmployeeIndexComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EmployeeIndexComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EmployeeIndexComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
