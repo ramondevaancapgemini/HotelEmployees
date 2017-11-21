66ae18a7f9a53164e8f11399ef11c11ca3685385
Emielvr <emielvr@hotmail.com>
2017-11-21 09:25:06 +0100

    Employee edit page

--- src/app/employee-edit/employee-edit.component.ts
+++ src/app/employee-edit/employee-edit.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Employee } from '../Employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-edit',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeEditComponent implements OnInit {
 
-  constructor() { }
+  @Input() employee: Employee;
+
+  constructor(private employeeService: EmployeeService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.employeeService.getEmployee(+id).subscribe(employee => this.employee = employee);
+    }
+  }
+
+  save(): void {
+    this.employeeService.updateEmployee(this.employee)
+      .subscribe(() => this.location.back());
   }
 
 }
--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -4,11 +4,13 @@ import { DashboardComponent } from '../dashboard/dashboard.component';
 import { EmployeeIndexComponent } from '../employee-index/employee-index.component';
 import { EmployeeDetailComponent } from '../employee-detail/employee-detail.component';
 import { EmployeeDeleteComponent } from '../employee-delete/employee-delete.component';
+import { EmployeeEditComponent } from '../employee-edit/employee-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: EmployeeDetailComponent },
+  { path: 'edit/:id', component: EmployeeEditComponent },
   { path: 'delete/:id', component: EmployeeDeleteComponent },
   { path: 'employees', component: EmployeeIndexComponent }
 ];
