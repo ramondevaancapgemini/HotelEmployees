8da7407abb2e7ad2e2889bedf22d46cb9a5d4315
Emielvr <emielvr@hotmail.com>
2017-11-22 10:42:28 +0100

    Fixed issue where user could bypass page limit

--- src/app/employee-index/employee-index.component.ts
+++ src/app/employee-index/employee-index.component.ts
@@ -12,16 +12,14 @@ import { Subject } from 'rxjs';
 })
 export class EmployeeIndexComponent implements OnInit {
 
-  dtOptions: DataTables.Settings = {};
   employees: Employee[];
-  dtTrigger: Subject<Employee> = new Subject();
 
   constructor(private employeeService: EmployeeService) { }
 
-  currentPage: number = 1;
   totalPages: number;
   pageLimit: number = 10;
-
+  currentPage: number = 1;
+  
   ngOnInit() {
     this.getEmployees();
   }
@@ -30,39 +28,25 @@ export class EmployeeIndexComponent implements OnInit {
     this.employeeService.getEmployees(page, limit)
       .subscribe(userData => {
         this.employees = userData.employees;
-        this.currentPage = userData.currentPage;
         this.totalPages = userData.totalPages;
         this.pageLimit = userData.pageLimit;
-        this.dtTrigger.next();
+        this.currentPage = Math.max(Math.min(userData.totalPages, userData.currentPage), 1);
       });
   }
 
   getPagination(): number[] {
     let min = Math.max(1, this.currentPage - 2);
     let max = Math.min(min + 4, this.totalPages);
-    return _.range(min, max + 1);
+    return _.range(min, max + 1); 
   }
 
   loadPage(page: number) {
+    page = Math.max(Math.min(this.totalPages, page), 1);
     this.getEmployees(page);
   }
 
   loadAmount(amount: number) {
     this.getEmployees(1, amount);
   }
-
-  //  add(name: string): void {
-  //    name = name.trim();
-  //    if (!name) { return; }
-  //    this.employeeService.addEmployee({ givenName } as Employee)
-  //      .subscribe(hero => {
-  //        this.heroes.push(hero);
-  //      });
-  //  }
-
-  // delete(employee: Employee): void {
-  //   this.employees = this.employees.filter(h => h !== employee);
-  //   this.employeeService.deleteEmployee(employee).subscribe();
-  // }
 
 }
