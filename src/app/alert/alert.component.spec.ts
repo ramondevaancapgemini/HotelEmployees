52f6ee872f89cccde67073cd5c33b2eb7c81127f
Emielvr <emielvr@hotmail.com>
2017-11-23 09:27:22 +0100

    Alert component tests

--- src/app/alert/alert.component.spec.ts
+++ src/app/alert/alert.component.spec.ts
@@ -1,7 +1,9 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {AlertComponent} from './alert.component';
-import {AlertService} from "../service/alert.service";
+import { AlertComponent } from './alert.component';
+import { AlertService } from "../service/alert.service";
+import { AlertType } from '../model/AlertType';
+import { Alert } from '../model/Alert';
 
 describe('AlertComponent', () => {
   let component: AlertComponent;
@@ -24,4 +26,40 @@ describe('AlertComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('when requesting a css class', () => {
+    it('should return nothing if no alert given', () => {
+      expect(component.cssClass(undefined)).toBeUndefined();
+    });
+    it('should return the correct class', () => {
+      let alert = { type: AlertType.Error, message: "test" };
+      expect(component.cssClass(alert)).toBe('alert alert-danger')
+    });
+  });
+
+  describe('when the array of alerts', () => {
+    it('is initialized it should be empty', () => {
+      expect(component.alerts.length).toBe(0);
+    });
+    describe('contains a new alert', () => {
+      let alert;
+      beforeEach(() => {
+        alert = { type: AlertType.Error, message: "test" };
+        component.alerts.push(alert);
+      });
+      it('the size should be 1', () => {
+        expect(component.alerts.length).toBe(1);
+      });
+      it('the size should be 1 after removing another alert', () => {
+        let anotherAlert = { type: AlertType.Warning, message: "test2" };
+        component.removeAlert(anotherAlert);
+        expect(component.alerts.length).toBe(1);
+      });
+      it('the size should be 0 after removing the alert', () => {
+        component.removeAlert(alert);
+        expect(component.alerts.length).toBe(0);
+      });
+    });
+  });
+
 });
